8c4e5964f31ec3a1fdd38a99de429712275f15e1
DumasOlivier <dumas.olivier@outlook.fr>
2020-02-07 19:20:27 +0100

    rubocop clean

--- spec/routing/keys_controller_spec.rb
+++ spec/routing/keys_controller_spec.rb
@@ -8,31 +8,31 @@ RSpec.describe RedisWebManager::KeysController, type: :routing do
   describe 'routing' do
     it 'routes to #index' do
       expect(get: '/keys').to(
-          route_to('redis_web_manager/keys#index')
+        route_to('redis_web_manager/keys#index')
       )
     end
 
     it 'routes to #show' do
       expect(get: '/key').to(
-          route_to('redis_web_manager/keys#show')
+        route_to('redis_web_manager/keys#show')
       )
     end
 
     it 'routes to #edit' do
       expect(get: '/key/edit').to(
-          route_to('redis_web_manager/keys#edit')
+        route_to('redis_web_manager/keys#edit')
       )
     end
 
     it 'routes to #update' do
       expect(put: '/keys').to(
-          route_to('redis_web_manager/keys#update')
+        route_to('redis_web_manager/keys#update')
       )
     end
 
     it 'routes to #destroy' do
       expect(delete: '/keys').to(
-          route_to('redis_web_manager/keys#destroy')
+        route_to('redis_web_manager/keys#destroy')
       )
     end
   end
