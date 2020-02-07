96e07d6af19287690b13a8773e83c518de0d50dd
DumasOlivier <dumas.olivier@outlook.fr>
2020-02-07 17:42:42 +0100

    data spec

---
+++ spec/redis_web_manager_data_spec.rb
@@ -0,0 +1,23 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+
+RSpec.describe RedisWebManager::Data do
+  let(:data) do
+    RedisWebManager::Data.new
+  end
+
+  describe 'data' do
+    it 'returns a OK (perform)' do
+      expect(data.perform).to eql('OK')
+    end
+
+    it 'returns a Array of keys' do
+      expect(data.keys).to be_a_kind_of(Array)
+    end
+
+    it 'returns a Array of keys deleted' do
+      expect(data.flush).to be_a_kind_of(Array)
+    end
+  end
+end
