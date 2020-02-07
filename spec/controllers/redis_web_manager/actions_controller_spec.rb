0cc32bd23cb0b87761bda581e511f4ebbc1cd01e
Benjamin Darcet <b.darcet@gmail.com>
2020-02-07 17:46:34 +0100

    add spec for flush action

--- spec/controllers/redis_web_manager/actions_controller_spec.rb
+++ spec/controllers/redis_web_manager/actions_controller_spec.rb
@@ -6,8 +6,16 @@ RSpec.describe RedisWebManager::ActionsController, type: :controller do
   routes { RedisWebManager::Engine.routes }
 
   describe 'DELETE #flushdb' do
+    it 'returns a success response' do
+      delete :flushdb
+      expect(response).to be_redirect
+    end
   end
 
   describe 'DELETE #flushall' do
+    it 'returns a success response' do
+      delete :flushall
+      expect(response).to be_redirect
+    end
   end
 end
